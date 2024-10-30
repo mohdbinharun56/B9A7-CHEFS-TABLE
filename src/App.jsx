0d54e40154fc233b5b7d4075f356f8adc744dfc8
mohdbinharun56 <arahaman461@gmail.com>
2024-10-30 15:06:34 +0600

    implement the cooking item delete if prepared

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
  
   const [recipes, setRecipes] = useState([]);
   const [cooking,setCooking] = useState([]);
+  const [prepareCook,setPrepareCook] = useState([]);
 
     useEffect(() => {
         fetch('recipesData.json')
@@ -20,19 +21,33 @@ function App() {
     const handleCook = (recipe)=>{
       const isExist = cooking.find(item=>item.recipe_id == recipe.recipe_id);
       if(!isExist){
+
         setCooking([...cooking,recipe]);
       }else{
         alert('This recipe is already cookig');
       }
-      
-
     }
     // console.log(cooking);
+
+     const handlePrepare = (item) => {
+
+        const isExist = prepareCook.find(prepare=>prepare.recipe_id==item.recipe_id);
+        if(!isExist){
+            const [item,...restItem] = cooking;
+            // console.log(restItem);
+            setCooking(restItem);
+            setPrepareCook([...prepareCook,item]);
+        }
+        else{
+            alert('Prepared to cook is already exist');
+        }
+    }
+
   return (
     <>
       <div className='mx-auto md:flex gap-4'>
         <Recipes recipes={recipes} handleCook={handleCook}></Recipes>
-        <Cooking cooking={cooking}></Cooking>
+        <Cooking cooking={cooking} prepareCook={prepareCook} handlePrepare={handlePrepare}></Cooking>
       </div>
     </>
   )
--- src/components/Cooking/Cooking.jsx
+++ src/components/Cooking/Cooking.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
-const Cooking = ({ cooking }) => {
+// import { useState } from "react";
+const Cooking = ({ cooking,handlePrepare,prepareCook }) => {
     // console.log(cooking);
+    // const [prepareCook,setPrepareCook] = useState([]);
+    
+    // console.log(prepareCook)
     return (
         <>
             <div className="w-1/3 border-2 rounded-2xl p-6">
-                <h1 className="text-center font-bold text-3xl mb-5">Want to cook: {cooking.length}</h1>
+                <h1 className="text-center font-bold text-xl mb-5">Want to cook: {cooking.length}</h1>
                 <hr />
                 <div className="mt-5">
                     <table className="w-full text-[#878787]">
@@ -17,33 +21,69 @@ const Cooking = ({ cooking }) => {
                             </tr>
                         </thead>
                         <tbody className="mt-2">
-                                {
-                                    cooking.map((item,idx) => {
-                                        console.log(item)
-                                        return (
-                                            <>
-                                                <tr className="font-normal text-sm bg-[#dad8d8]">
+                            {
+                                cooking.map((item, idx) => {
+                                    console.log(item)
+                                    return (
+                                        <>
+                                            <tr className="font-normal text-sm bg-[#f5f3f3] h-20">
 
-                                                    <td>{idx+1}</td>
-                                                    <td className="w-40 text-start pl-2">{item.recipe_name}</td>
-                                                    <td className="text-center pl-2">{item.preparing_time}</td>
-                                                    <td className="text-end pr-2 w-28">{item.calories}</td>
-                                                    <td className="w-20">
-                                                        <button className="bg-[#0BE58A] h-10 text-[#282828] p-2 rounded-full">Preparing</button>
-                                                    </td>
-                                                </tr>
-                                            </>
-                                        );
-                                    })
-                                }
+                                                <td>{idx + 1}</td>
+                                                <td className="w-40 text-start pl-2">{item.recipe_name}</td>
+                                                <td className="text-center pl-2">{item.preparing_time}</td>
+                                                <td className="text-end w-28">{item.calories}</td>
+                                                <td className="w-20">
+                                                    <button onClick={() => handlePrepare(item)} className="bg-[#0BE58A] h-10 text-[#282828] p-2 rounded-full">Preparing</button>
+                                                </td>
+                                            </tr>
+                                        </>
+                                    );
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
+
+                <h1 className="text-center font-bold text-xl mb-5 mt-20">Currently cooking: {prepareCook.length}</h1>
+                <hr />
+                <div className="mt-5">
+                    <table className="w-full text-[#878787]">
+                        <thead>
+                            <tr>
+                                <td></td>
+                                <td className="text-start pl-2">Name</td>
+                                <td className="text-center pl-2">Time</td>
+                                <td className="text-end pr-2">Calories</td>
+                            </tr>
+                        </thead>
+                        <tbody className="mt-2">
+                            {
+                                prepareCook.map((item,idx) => {
+                                    console.log(item)
+                                    return (
+                                        <>
+                                            <tr className="font-normal text-sm bg-[#f5f3f3] h-20">
+
+                                                <td>{idx+1}</td>
+                                                <td className="w-40 text-start pl-2">{item.recipe_name}</td>
+                                                <td className="text-center pl-2">{item.preparing_time}</td>
+                                                <td className="text-end w-28">{item.calories}</td>
+                                            </tr>
+                                        </>
+                                    );
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </>
     );
 };
 Cooking.propTypes = {
-    cooking: PropTypes.array
+    cooking: PropTypes.array,
+    handlePrepare: PropTypes.func,
+    prepareCook:PropTypes.array
 }
 export default Cooking;
